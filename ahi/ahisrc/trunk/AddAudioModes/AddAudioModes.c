2edb77181da4d9a7cc4e79896e3e68962a6d1ae3
lcs <lcs@4f48108a-62f3-0310-b161-d5a2fa85e521>
1997-01-04 00:24:51 +0000

    Added DBLSCAN switch + some other small changes

--- ahi/ahisrc/trunk/AddAudioModes/AddAudioModes.c
+++ ahi/ahisrc/trunk/AddAudioModes/AddAudioModes.c
@@ -1,89 +1,135 @@
+/* $Id$
+* $Log$
+* Revision 1.2  1997/01/04 00:24:51  lcs
+* Added DBLSCAN switch + some other small changes
+*
+*/
 
 #include <devices/ahi.h>
-#include <proto/exec.h>
-#include <proto/dos.h>
+#include <graphics/modeid.h>
+#include <intuition/screens.h>
 #include <proto/ahi.h>
+#include <proto/dos.h>
+#include <proto/exec.h>
+#include <proto/graphics.h>
+#include <proto/intuition.h>
 #include <stdlib.h>
 
-struct Library    *AHIBase;
-struct MsgPort    *AHImp=NULL;
-struct AHIRequest *AHIio=NULL;
-BYTE               AHIDevice=-1;
+void OpenAHI(void);
+void cleanup(LONG);
+LONG main(void);
+
+struct Library    *AHIBase   = NULL;
+struct MsgPort    *AHImp     = NULL;
+struct AHIRequest *AHIio     = NULL;
+BYTE               AHIDevice = -1;
 
 #define AHIVERSION 3
 
 LONG __OSlibversion=37;
 
-const static UBYTE version[]="$VER: AddAudioModes 1.1 "__AMIGADATE__"\n\r";
+const static UBYTE version[]="$VER: AddAudioModes 1.2 "__AMIGADATE__"\n\r";
 
-void cleanup(LONG rc)
-{
-  if(!AHIDevice)
+void OpenAHI(void) {
+  if(AHIDevice) {
+    if(AHImp=CreateMsgPort()) {
+      if(AHIio=(struct AHIRequest *)CreateIORequest(AHImp,sizeof(struct AHIRequest))) {
+        AHIio->ahir_Version=AHIVERSION;
+        AHIDevice=OpenDevice(AHINAME,AHI_NO_UNIT,(struct IORequest *)AHIio,NULL);
+      }
+    }
+
+    if(AHIDevice) {
+      Printf("Unable to open %s version %d\n",AHINAME,AHIVERSION);
+      cleanup(RETURN_FAIL);
+    }
+
+    AHIBase=(struct Library *)AHIio->ahir_Std.io_Device;
+  }
+}
+
+void cleanup(LONG rc) {
+  if(!AHIDevice) {
     CloseDevice((struct IORequest *)AHIio);
+  }
   DeleteIORequest((struct IORequest *)AHIio);
   DeleteMsgPort(AHImp);
   exit(rc);
 }
 
-struct
-{
+#define TEMPLATE "FILES/M,QUIET/S,REFRESH/S,REMOVE/S,DBLSCAN/S"
+
+struct {
   STRPTR *files;
   ULONG   quiet;
   ULONG   refresh;
   ULONG   remove;
-} args = {NULL, FALSE, FALSE, FALSE};
+  ULONG   dblscan;
+} args = {NULL, FALSE, FALSE, FALSE, FALSE};
 
-LONG main(void)
-{
+LONG main(void) {
   struct RDArgs *rdargs;
   ULONG  i=0,id,rc=RETURN_OK;
 
-  if(AHImp=CreateMsgPort())
-    if(AHIio=(struct AHIRequest *)CreateIORequest(AHImp,sizeof(struct AHIRequest))) {
-      AHIio->ahir_Version=AHIVERSION;
-      AHIDevice=OpenDevice(AHINAME,AHI_NO_UNIT,(struct IORequest *)AHIio,NULL);
-      }
+  if(rdargs=ReadArgs( TEMPLATE , (LONG *) &args, NULL)) {
 
-  if(AHIDevice) {
-    Printf("Unable to open %s version %d\n",AHINAME,AHIVERSION);
-    cleanup(RETURN_FAIL);
-  }
-  AHIBase=(struct Library *)AHIio->ahir_Std.io_Device;
-
-  if(rdargs=ReadArgs("FILES/M,QUIET/S,REFRESH/S,REMOVE/S", (LONG *) &args, NULL))
-  {
-    if(args.refresh && !args.remove)
-    {
-      if(!AHI_LoadModeFile("DEVS:AudioModes") && !args.quiet)
-      {
+    // Refresh database
+    if(args.refresh && !args.remove) {
+      OpenAHI();
+      if(!AHI_LoadModeFile("DEVS:AudioModes") && !args.quiet) {
         PrintFault(IoErr(),"DEVS:AudioModes");
         rc=RETURN_ERROR;
       }
-      
     }
-    if(args.files && !args.remove)
-    {
-      while(args.files[i])
-      {
-        if(!AHI_LoadModeFile(args.files[i]) && !args.quiet)
-        {
+
+    // Load mode files
+    if(args.files && !args.remove) {
+      OpenAHI();
+      while(args.files[i]) {
+        if(!AHI_LoadModeFile(args.files[i]) && !args.quiet) {
           PrintFault(IoErr(),args.files[i]);
           rc=RETURN_ERROR;
         }
         i++;
       }
     }
-    if(args.remove)
-    {
-      if(args.files || args.refresh)
-      {
+
+    // Remove database
+    if(args.remove) {
+      if(args.files || args.refresh) {
         PutStr("The REMOVE switch cannot be used together with FILES or REFRESH.\n");
         rc=RETURN_FAIL;
       }
-      else
-      {
-        while((id=AHI_NextAudioID(AHI_INVALID_ID)) != AHI_INVALID_ID)
+      else {
+        OpenAHI();
+        while((id=AHI_NextAudioID(AHI_INVALID_ID)) != AHI_INVALID_ID) {
           AHI_RemoveAudioMode(id);
+        }
+      }
+    }
+
+    // Make display mode doublescan (allowing > 28 kHz sample rates)
+    if(args.dblscan) {
+      ULONG id;
+      struct Screen *screen = NULL;
+      const static struct ColorSpec colorspecs[] = {
+        { 0, 0, 0, 0 },
+        { 1, 0, 0, 0 },
+        {-1, 0, 0, 0 }
+      };
+      
+      id = BestModeID(
+          BIDTAG_DIPFMustHave, DIPF_IS_SCANDBL,
+          BIDTAG_DIPFMustNotHave, DIPF_IS_FOREIGN,
+          TAG_DONE);
+      if(id != INVALID_ID) {
+        screen = OpenScreenTags(NULL,
+            SA_DisplayID,  id,
+            SA_Colors,    &colorspecs,
+            TAG_DONE);
+      }
+      if(screen) {
+        CloseScreen(screen);
       }
     }
     FreeArgs(rdargs);
