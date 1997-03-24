154b76636c40b8063fe1a138046384fc0df5fa06
lcs <lcs@4f48108a-62f3-0310-b161-d5a2fa85e521>
1997-03-24 12:43:09 +0000

    Initial revision

---
+++ ahi/ahisrc/trunk/Device/dsp.h
@@ -0,0 +1,31 @@
+/* $Id$
+* $Log$
+* Revision 1.1  1997/03/24 12:43:09  lcs
+* Initial revision
+*
+*/
+
+#ifndef DSP_H
+#define DSP_H
+
+#include <exec/types.h>
+
+struct Echo
+{
+	ULONG	ahiecho_Delay;
+	void	(*ahiecho_Code)(void);	// The echo routine
+	LONG	ahiecho_FeedbackDS;	// Delayed signal to same channel
+	LONG	ahiecho_FeedbackDO;	// Delayed signal to other channel
+	LONG	ahiecho_FeedbackNS;	// Normal signal to same channel
+	LONG	ahiecho_FeedbackNO;	// Normal signal to other channel
+	LONG	ahiecho_MixN;		// Normal signal
+	LONG	ahiecho_MixD;		// Delayed signal
+	ULONG	ahiecho_Offset;		// (&Buffer-&SrcPtr)/sizeof(ahiecho_Buffer[0])
+	APTR	ahiecho_SrcPtr;		// Pointer to &Buffer
+	APTR	ahiecho_DstPtr;		// Pointer to &(Buffer[Delay])
+	APTR	ahiecho_EndPtr;		// Pointer to address after buffer
+	ULONG	ahiecho_BufferSize;	// Delay buffer size in bytes
+	BYTE	ahiecho_Buffer[0];	// Delay buffer
+};
+
+#endif
---
+++ ahi/ahisrc/trunk/Device/effectinit.c
@@ -0,0 +1,188 @@
+/* $Id$
+* $Log$
+* Revision 1.1  1997/03/24 12:41:51  lcs
+* Initial revision
+*
+*/
+
+#include <exec/memory.h>
+#include <proto/exec.h>
+
+#include "ahi_def.h"
+#include "dsp.h"
+
+#ifndef _GENPROTO
+#include "dspinit_protos.h"
+#endif
+
+__asm extern void do_DSPEchoMono16(void);
+__asm extern void do_DSPEchoMono16Fast(void);
+__asm extern void do_DSPEchoStereo16(void);
+__asm extern void do_DSPEchoStereo16Fast(void);
+__asm extern void do_DSPEchoMono32(void);
+__asm extern void do_DSPEchoStereo32(void);
+__asm extern void do_DSPEchoMono16NCFM(void);
+__asm extern void do_DSPEchoStereo16NCFM(void);
+__asm extern void do_DSPEchoMono16NCFMFast(void);
+__asm extern void do_DSPEchoStereo16NCFMFast(void);
+
+__asm extern LONG Fixed2Shift(register __d0 Fixed);
+
+__asm extern void update_MasterVolume(register __a2 struct AHIPrivAudioCtrl *);
+
+/**
+*** DSPECHO
+**/
+
+// NOTE: The mixing routine might execute while we are inside these
+// functions!
+
+__asm BOOL update_DSPEcho(
+    register __a0 struct AHIDSPEcho *echo,
+    register __a2 struct AHIPrivAudioCtrl *actrl,
+    register __a5 struct AHIBase *AHIBase)
+{
+  ULONG length;
+  struct Echo *es;
+
+  free_DSPEcho(actrl, AHIBase);
+
+  length = AHI_SampleFrameSize(actrl->ac.ahiac_BuffType) *
+           (echo->ahiede_Delay + actrl->ac.ahiac_MaxBuffSamples) +
+           sizeof(struct Echo);
+
+  es = AllocVec(length, MEMF_PUBLIC|MEMF_CLEAR);
+  
+  if(actrl->ahiac_EffDSPEchoStruct)
+  {
+    ULONG mode = 0;
+#define mode_stereo 1
+#define mode_32bit  2
+#define mode_ncnm   4       // No cross, no mix
+#define mode_fast   8
+
+    es->ahiecho_BufferSize = length;
+    es->ahiecho_EndPtr     = es->ahiecho_Buffer + length;
+    es->ahiecho_SrcPtr     = es->ahiecho_Buffer;
+    es->ahiecho_DstPtr     = es->ahiecho_Buffer +
+        (AHI_SampleFrameSize(actrl->ac.ahiac_BuffType) * echo->ahiede_Delay);
+
+    switch(actrl->ac.ahiac_BuffType)
+    {
+      case AHIST_M16S:
+        echo->ahiede_Cross = 0;
+        break;
+      case AHIST_S16S:
+        mode |= mode_stereo;
+        break;
+      case AHIST_M32S:
+        echo->ahiede_Cross = 0;
+        mode |= mode_32bit;
+        break;
+      case AHIST_S32S:
+        mode |= (mode_32bit | mode_stereo);
+        break;
+    }
+
+    es->ahiecho_Delay      = echo->ahiede_Delay;
+    es->ahiecho_MixD       = echo->ahiede_Mix;
+    es->ahiecho_MixN       = 0x10000 - echo->ahiede_Mix;
+    es->ahiecho_FeedbackDO = (echo->ahiede_Feedback >> 8) *
+                             (echo->ahiede_Cross >> 8);
+    es->ahiecho_FeedbackDS = (echo->ahiede_Feedback >> 8) *
+                             ((0x10000 - echo->ahiede_Cross) >> 8);
+    es->ahiecho_FeedbackNO = ((0x10000 - echo->ahiede_Feedback) >> 8) *
+                             (echo->ahiede_Cross >> 8);
+    es->ahiecho_FeedbackNS = ((0x10000 - echo->ahiede_Feedback) >> 8) *
+                             ((0x10000 - echo->ahiede_Cross) >> 8);
+
+
+    if((echo->ahiede_Cross == 0) && (echo->ahiede_Mix == 0x10000))
+    {
+      mode |= mode_ncnm;
+      actrl->ahiac_EchoMasterVolume = es->ahiecho_FeedbackNS;
+    }
+    else
+    {
+      actrl->ahiac_EchoMasterVolume = 0x10000;
+    }
+
+    update_MasterVolume(actrl);
+
+    if(AHIBase->ahib_Flags & AHIBF_FASTECHO)
+    {
+      es->ahiecho_MixD = Fixed2Shift(es->ahiecho_MixD);
+      es->ahiecho_MixN = Fixed2Shift(es->ahiecho_MixN);
+      es->ahiecho_FeedbackDO = Fixed2Shift(es->ahiecho_FeedbackDO);
+      es->ahiecho_FeedbackDS = Fixed2Shift(es->ahiecho_FeedbackDS);
+      es->ahiecho_FeedbackNO = Fixed2Shift(es->ahiecho_FeedbackNO);
+      es->ahiecho_FeedbackNS = Fixed2Shift(es->ahiecho_FeedbackNS);
+      mode |= mode_fast;
+    }
+
+    switch(mode)
+    {
+      case 0:
+        es->ahiecho_Code   = (void (*)(void)) do_DSPEchoMono16;
+        break;
+      case 1:
+        es->ahiecho_Code   = (void (*)(void)) do_DSPEchoStereo16;
+        break;
+      case 2:
+        es->ahiecho_Code   = (void (*)(void)) do_DSPEchoMono32;
+        break;
+      case 3:
+        es->ahiecho_Code   = (void (*)(void)) do_DSPEchoStereo32;
+        break;
+      case 4:
+        es->ahiecho_Code   = (void (*)(void)) do_DSPEchoMono16NCFM;
+        break;
+      case 5:
+        es->ahiecho_Code   = (void (*)(void)) do_DSPEchoStereo16NCFM;
+        break;
+      case 6:
+        es->ahiecho_Code   = (void (*)(void)) do_DSPEchoMono32;
+        break;
+      case 7:
+        es->ahiecho_Code   = (void (*)(void)) do_DSPEchoStereo32;
+        break;
+      case 8:
+        es->ahiecho_Code   = (void (*)(void)) do_DSPEchoMono16Fast;
+        break;
+      case 9:
+        es->ahiecho_Code   = (void (*)(void)) do_DSPEchoStereo16Fast;
+        break;
+      case 10:
+        es->ahiecho_Code   = (void (*)(void)) do_DSPEchoMono32;
+        break;
+      case 11:
+        es->ahiecho_Code   = (void (*)(void)) do_DSPEchoStereo32;
+        break;
+      case 12:
+        es->ahiecho_Code   = (void (*)(void)) do_DSPEchoMono16NCFMFast;
+        break;
+      case 13:
+        es->ahiecho_Code   = (void (*)(void)) do_DSPEchoStereo16NCFMFast;
+        break;
+    }
+
+    // Structure filled, make it available to the mixing routine
+    actrl->ahiac_EffDSPEchoStruct = es;
+  }
+  return FALSE;
+}
+
+
+__asm void free_DSPEcho(
+    register __a2 struct AHIPrivAudioCtrl *actrl,
+    register __a5 struct AHIBase *AHIBase)
+{
+  void *p = actrl->ahiac_EffDSPEchoStruct;
+
+  // Hide it from mixing routine before freeing it!
+  actrl->ahiac_EffDSPEchoStruct = NULL;
+  FreeVec(p);
+
+  actrl->ahiac_EchoMasterVolume = 0x10000;
+  update_MasterVolume(actrl);
+}
