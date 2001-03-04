67d8e6875a6246494c5760b7d784b456650332c4
lcs <lcs@4f48108a-62f3-0310-b161-d5a2fa85e521>
2001-03-04 19:45:05 +0000

    Fixed copyright year (from 2000 to 2001).

--- ahi/ahisrc/trunk/Device/header.c
+++ ahi/ahisrc/trunk/Device/header.c
@@ -2,7 +2,7 @@
 
 /*
      AHI - Hardware independent audio subsystem
-     Copyright (C) 1996-2000 Martin Blom <martin@blom.org>
+     Copyright (C) 1996-2001 Martin Blom <martin@blom.org>
      
      This library is free software; you can redistribute it and/or
      modify it under the terms of the GNU Library General Public
@@ -152,7 +152,7 @@ const char DevName[]   = AHINAME;
 const char IDString[]  = AHINAME " " VERS "\r\n";
 
 static const char VersTag[] =
- "$VER: " AHINAME " " VERS " ©1994-2000 Martin Blom. "
+ "$VER: " AHINAME " " VERS " ©1994-2001 Martin Blom. "
  CPU 
  " version.\r\n";
 
