50deb730000ae9d9c96963372fddf4e4dcf4dd12
Martin Blom <martin@blom.org>
2002-01-03 08:56:24 +0000

    Added license notice.

--- Toccata/libinit.c
+++ Toccata/libinit.c
@@ -1,6 +1,30 @@
+/* $Id$ */
+
 /*
- * $Id$
+     toccata.library - AHI-based Toccata emulation library
+     Copyright (C) 1997-2002 Martin Blom <martin@blom.org> and Teemu Suikki.
+     
+     This library is free software; you can redistribute it and/or
+     modify it under the terms of the GNU Library General Public
+     License as published by the Free Software Foundation; either
+     version 2 of the License, or (at your option) any later version.
+     
+     This library is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+     Library General Public License for more details.
+     
+     You should have received a copy of the GNU Library General Public
+     License along with this library; if not, write to the
+     Free Software Foundation, Inc., 59 Temple Place - Suite 330, Cambridge,
+     MA 02139, USA.
+*/
+
+/*
  * $Log$
+ * Revision 1.3  2002/01/03 08:56:24  martin
+ * Added license notice.
+ *
  * Revision 1.2  1997/06/25 19:55:04  lcs
  * Modified to make the library base compatible with the public
  * part of ToccataBase.
