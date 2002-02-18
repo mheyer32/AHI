b26844cc43aaefdd88e59e953055e1176718d641
Martin Blom <martin@blom.org>
2002-02-18 16:11:24 +0000

    *** empty log message ***

--- Drivers/EMU10kx/EMU10kx.h
+++ Drivers/EMU10kx/EMU10kx.h
@@ -2,14 +2,27 @@
 #ifndef AHI_Drivers_EMU10k1
 #define AHI_Drivers_EMU10k1
 
-struct EMU10k1
+#include <exec/types.h>
+
+
+typedef unsigned int   u32;
+typedef unsigned short u16;
+typedef unsigned char  u8;
+
+typedef signed int     s32;
+typedef signed short   s16;
+typedef signed char    s8;
+
+struct emu10k1_card
 {
-    APTR   m_PCIDev;
-    UBYTE* m_IOBase;
-    ULONG  m_IOLength;
-    UBYTE  m_IRQ;
-    UBYTE  m_ChipRev;
-    UWORD  m_Model;
+    APTR   pci_dev;
+    ULONG  iobase;
+    ULONG  length;
+    UBYTE  irq;
+    UBYTE  chiprev;
+    UWORD  model;
+    BOOL   audigy;
+    BOOL   isaps;
 };
 
 #endif /* AHI_Drivers_EMU10k1 */
