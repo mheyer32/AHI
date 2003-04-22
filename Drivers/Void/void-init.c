da2f7dfcd53d0f3ac0d52f3c249921c5e74a09a8
Martin Blom <martin@blom.org>
2003-04-22 14:08:33 +0000

    This example driver can now be compiled using gcc and builds for all targets (m68k-amigaos, ppc-morphos, i?86be-amithlon and i386-aros).

---
+++ Drivers/Void/void-init.c
@@ -0,0 +1,44 @@
+
+#include <config.h>
+
+#include "library.h"
+
+/******************************************************************************
+** Custom driver init *********************************************************
+******************************************************************************/
+
+BOOL
+DriverInit( struct DriverBase* AHIsubBase )
+{
+  AHIsubBase->dosbase       = OpenLibrary( DOSNAME, 37 );
+
+  if( DOSBase == NULL )
+  {
+    Req( "Unable to open 'dos.library' version 37.\n" );
+    return FALSE;
+  }
+  
+  // Fail if no hardware is present (this check prevents the audio
+  // modes from being added to the database if the driver cannot be
+  // used).
+
+/*
+  if( unable_to_find_hardware )
+  {
+    Req( "No sound card present.\n" );
+    return FALSE;
+  }
+*/
+  return TRUE;
+}
+
+
+/******************************************************************************
+** Custom driver clean-up *****************************************************
+******************************************************************************/
+
+VOID
+DriverCleanup( struct DriverBase* AHIsubBase )
+{
+  CloseLibrary( (struct Library*) DOSBase );
+}
