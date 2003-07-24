146c73cfcdd0d619812b0eeefe5211c2678ee2be
martin <martin@4f48108a-62f3-0310-b161-d5a2fa85e521>
2003-07-24 21:18:35 +0000

    Now uses the macros for correct endianess.

--- ahi/ahisrc/trunk/Drivers/EMU10kx/EMU10KX.s
+++ ahi/ahisrc/trunk/Drivers/EMU10kx/EMU10KX.s
@@ -7,27 +7,27 @@
 	
 	CHUNK_START	AUDM
 1:	
-	.long		AHIDB_AudioID,	0x001e0001
-	.long		AHIDB_Volume,	TRUE
-	.long		AHIDB_Panning,	FALSE
-	.long		AHIDB_Stereo,	FALSE
-	.long		AHIDB_HiFi,	TRUE
-	.long		AHIDB_MultTable,FALSE
-	.long		AHIDB_Name,	2f-1b
-	.long		TAG_DONE
+	LONG2		AHIDB_AudioID,	0x001e0001
+	LONG2		AHIDB_Volume,	TRUE
+	LONG2		AHIDB_Panning,	FALSE
+	LONG2		AHIDB_Stereo,	FALSE
+	LONG2		AHIDB_HiFi,	TRUE
+	LONG2		AHIDB_MultTable,FALSE
+	LONG2		AHIDB_Name,	2f-1b
+	LONG		TAG_DONE
 2:
 	.asciz		"EMU10kx:HiFi 16 bit mono"
 	CHUNK_END
 
 	CHUNK_START	AUDM
 1:	
-	.long		AHIDB_AudioID,	0x001e0002
-	.long		AHIDB_Volume,	TRUE
-	.long		AHIDB_Panning,	TRUE
-	.long		AHIDB_Stereo,	TRUE
-	.long		AHIDB_HiFi,	TRUE
-	.long		AHIDB_MultTable,FALSE
-	.long		AHIDB_Name,	2f-1b
+	LONG2		AHIDB_AudioID,	0x001e0002
+	LONG2		AHIDB_Volume,	TRUE
+	LONG2		AHIDB_Panning,	TRUE
+	LONG2		AHIDB_Stereo,	TRUE
+	LONG2		AHIDB_HiFi,	TRUE
+	LONG2		AHIDB_MultTable,FALSE
+	LONG2		AHIDB_Name,	2f-1b
 	.long		TAG_DONE
 2:
 	.asciz		"EMU10kx:HiFi 16 bit stereo++"
@@ -35,28 +35,28 @@
 		
 	CHUNK_START	AUDM
 1:	
-	.long		AHIDB_AudioID,	0x001e0003
-	.long		AHIDB_Volume,	TRUE
-	.long		AHIDB_Panning,	FALSE
-	.long		AHIDB_Stereo,	FALSE
-	.long		AHIDB_HiFi,	FALSE
-	.long		AHIDB_MultTable,FALSE
-	.long		AHIDB_Name,	2f-1b
-	.long		TAG_DONE
+	LONG2		AHIDB_AudioID,	0x001e0003
+	LONG2		AHIDB_Volume,	TRUE
+	LONG2		AHIDB_Panning,	FALSE
+	LONG2		AHIDB_Stereo,	FALSE
+	LONG2		AHIDB_HiFi,	FALSE
+	LONG2		AHIDB_MultTable,FALSE
+	LONG2		AHIDB_Name,	2f-1b
+	LONG		TAG_DONE
 2:
 	.asciz		"EMU10kx:16 bit mono"
 	CHUNK_END
 
 	CHUNK_START	AUDM
 1:	
-	.long		AHIDB_AudioID,	0x001e0004
-	.long		AHIDB_Volume,	TRUE
-	.long		AHIDB_Panning,	TRUE
-	.long		AHIDB_Stereo,	TRUE
-	.long		AHIDB_HiFi,	FALSE
-	.long		AHIDB_MultTable,FALSE
-	.long		AHIDB_Name,	2f-1b
-	.long		TAG_DONE
+	LONG2		AHIDB_AudioID,	0x001e0004
+	LONG2		AHIDB_Volume,	TRUE
+	LONG2		AHIDB_Panning,	TRUE
+	LONG2		AHIDB_Stereo,	TRUE
+	LONG2		AHIDB_HiFi,	FALSE
+	LONG2		AHIDB_MultTable,FALSE
+	LONG2		AHIDB_Name,	2f-1b
+	LONG		TAG_DONE
 2:
 	.asciz		"EMU10kx:16 bit stereo++"
 	CHUNK_END
