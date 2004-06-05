9509920a5224e1cb23a74730cd144dc8466a6d58
Martin Blom <martin@blom.org>
2004-06-05 22:15:40 +0000

    Added class ahi-buffer.class

---
+++ Classes/Buffer/ahiclass.h
@@ -0,0 +1,26 @@
+#ifndef AHI_Classes_Buffer_ahiclass_h
+#define AHI_Classes_Buffer_ahiclass_h
+
+#include <classes/ahi_types.h>
+
+#include "boopsi.h"
+
+struct AHIClassBase {
+    struct CommonBase    common;
+    struct ClassLibrary* super;
+};
+
+struct AHIClassData {
+    ULONG             sample_type;
+    ULONG             capacity;
+    ULONG             length;
+    APTR              data;
+    ULONG             sample_freq_int;
+    ULONG             sample_freq_fract;
+    ULONG             timestamp_hi;
+    ULONG             timestamp_lo;
+    ULONG             age_hi;
+    ULONG             age_lo;
+};
+
+#endif /* AHI_Classes_Buffer_ahiclass_h */
