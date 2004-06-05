25075ccf303b137c49498078e4d96df21c3219cb
Martin Blom <martin@blom.org>
2004-06-05 12:24:01 +0000

    Added initial skeleton for AHI classes.

---
+++ Classes/Common/boopsi.h
@@ -0,0 +1,36 @@
+#ifndef AHI_Classes_Common_boopsi_h
+#define AHI_Classes_Common_boopsi_h
+
+#include <exec/types.h>
+#include <proto/exec.h>
+#include <proto/intuition.h>
+#include <proto/utility.h>
+
+extern const char  ClassName[];
+extern const char  ClassIDString[];
+extern const UWORD ClassVersion;
+extern const UWORD ClassRevision;
+
+struct CommonBase {
+    struct ClassLibrary cl;
+    BPTR                seglist;
+};
+
+struct CommonData {
+};
+
+struct AHIClassBase;
+struct AHIClassData;
+
+BOOL
+AHIClassInit(struct AHIClassBase* AHIClassBase);
+
+VOID
+AHIClassCleanup(struct AHIClassBase* AHIClassBase);
+
+ULONG
+AHIClassDispatch(Class*  class,
+		 Object* object,
+		 Msg     msg);
+
+#endif /* AHI_Classes_Common_boopsi_h */
