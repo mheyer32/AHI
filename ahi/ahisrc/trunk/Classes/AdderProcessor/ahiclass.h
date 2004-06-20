039ed212aa284d3d830cf036827a9608730c3214
martin <martin@4f48108a-62f3-0310-b161-d5a2fa85e521>
2004-06-20 19:58:51 +0000

    Added AdderProcessor.

---
+++ ahi/ahisrc/trunk/Classes/AdderProcessor/ahiclass.h
@@ -0,0 +1,26 @@
+#ifndef AHI_Classes_MyProcessor_ahiclass_h
+#define AHI_Classes_MyProcessor_ahiclass_h
+
+#include <classes/ahi_types.h>
+#include <exec/lists.h>
+
+#include "boopsi.h"
+
+struct AHIClassBase {
+    struct CommonBase    common;
+    struct ClassLibrary* super;
+};
+
+#define MAX_CHILDREN 64
+
+struct AHIClassData {
+    ULONG   channels;
+    Object* buffer;
+    float*  data;
+    ULONG   length;
+    Object* children[MAX_CHILDREN];
+    Object* buffers[MAX_CHILDREN];
+    float*  datas[MAX_CHILDREN];
+};
+
+#endif /* AHI_Classes_MyProcessor_ahiclass_h */
