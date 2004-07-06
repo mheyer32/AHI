37cc9de496bc0d69d5ef4bfa25ff67b0f1b0beb5
Martin Blom <martin@blom.org>
2004-07-06 19:34:42 +0000

    Added ahi-board.class.

---
+++ Classes/Board/ahiclass.h
@@ -0,0 +1,17 @@
+#ifndef AHI_Classes_Board_ahiclass_h
+#define AHI_Classes_Board_ahiclass_h
+
+#include <classes/ahi_types.h>
+#include <exec/lists.h>
+
+#include "boopsi.h"
+
+struct AHIClassBase {
+    struct CommonBase    common;
+    struct ClassLibrary* super;
+};
+
+struct AHIClassData {
+};
+
+#endif /* AHI_Classes_Board_ahiclass_h */
