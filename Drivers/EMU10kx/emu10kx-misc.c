7c8356a5031ca75fcd58045356be36019db16ed4
Martin Blom <martin@blom.org>
2005-03-24 11:24:07 +0000

    * emu10kx.audio: Removed unnecessary OS4-only interrupt disable code.

--- Drivers/EMU10kx/emu10kx-misc.c
+++ Drivers/EMU10kx/emu10kx-misc.c
@@ -112,11 +112,6 @@ AllocDriverData( APTR               dev,
   dd->card.is_audigy = ( ahi_pci_read_config_word(PCI_DEVICE_ID, dev) == PCI_DEVICE_ID_CREATIVE_AUDIGY );
   dd->card.is_aps   = ( ahi_pci_read_config_long( PCI_SUBSYSTEM_VENDOR_ID, dev) == EMU_APS_SUBID );
 
-#ifdef __AMIGAOS4__
-  ahi_pci_outl(0xffffffff, dd->card.iobase + IPR, dev);
-  ahi_pci_outl(0, dd->card.iobase + INTE, dev);
-#endif
-
   ahi_pci_add_intserver( &dd->interrupt, dev);
 
   dd->interrupt_added = TRUE;
