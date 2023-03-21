ab61efd395225e76aae18f3309921eccd41ce269
mheyer <sonode@gmx.de>
2023-03-20 21:52:30 -0700

    Build OpenPCI as part of build_externals.sh
    Strictly speaking, we don't need it since the build
    of AHI is just dependent on its headers. But just in case,
    it can't hurt either.
    
    Build OpenPCI, prometheus.library and prometheus.card in build_externals.sh

--- build_externals.sh
+++ build_externals.sh
@@ -1,7 +1,10 @@
-#!/bin/sh
+#!/bin/sh 
+
+#echo on
+set -x
 
 # makeinfo in guise of mkguide
- make -C ./external/mkguide/src all -B
+make -C ./external/mkguide/src all
 
 # flexcat
 make -C ./external/flexcat bootstrap OS=unix
@@ -10,3 +13,15 @@ make -C ./external/flexcat OS=unix bin_unix/flexcat
 
 #robodoc
 make -C ./external/robodoc
+
+#OpenPCI
+make -C ./external/OpenPCI -f makefile_m68k
+
+#Prometheus.card
+make -C ./external/Prometheus/PrometheusCard -f makefiles/gcc/makefile
+cp ./external/Prometheus/PrometheusCard/_bin/gcc/Prometheus.card /media/sf_Amiga/
+
+#Prometheus.library / exe
+make -C ./external/Prometheus/PromLib -f makefiles/gcc/makefile
+cp ./external/Prometheus/PromLib/_bin/gcc/prometheus.library /media/sf_Amiga/
+cp ./external/Prometheus/PromLib/_bin/gcc/prometheus.exe /media/sf_Amiga/
